aff7464a9b7f2439b562223f5d3f23d2f8b162d8
DESKTOP-FJPDUTL\user <zxc73226>
2017-08-28 18:57:37 +0800

    進度更新

--- js/Exercise.js
+++ js/Exercise.js
@@ -1,20 +1,17 @@
 $(document).ready(function () {
-  var yudan = "";
-  var now = new Date();
-  var month = now.getMonth() + 1;
-  var date = now.getDate();
-  var year = now.getYear();
-  if (year < 2000) year = year + 1900;
-  document.write(year + " 年 " + yudan + month + " 月 " + date + " 日 ");
-  function ShowTime() {
-    var NowDate = new Date();
-    var h = NowDate.getHours();
-    var m = NowDate.getMinutes();
-    var s = NowDate.getSeconds();
-    document.getElementById('showbox').innerHTML = h + '時' + m + '分' + s + '秒';
-    setTimeout('ShowTime()', 1000);
+  function current() {
+    var d = new Date(), str = '';
+    str += d.getFullYear() + '年'; //獲取當前年份 
+    str += d.getMonth() + 1 + '月'; //獲取當前月份（0——11） 
+    str += d.getDate() + '日';
+    str += d.getHours() + '時';
+    str += d.getMinutes() + '分';
+    str += d.getSeconds() + '秒';
+    return str;
   }
-
+  setInterval(function () { $("#nowTime").html(current) }, 1000);
 });
+
+
 
 
