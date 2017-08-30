38f01dee22928bac656c9fa385fbea86a9581081
DESKTOP-FJPDUTL\user <zxc73226>
2017-08-30 18:48:53 +0800

    js 進度更新

--- js/Exercise.js
+++ js/Exercise.js
@@ -1,17 +1,3 @@
 $(document).ready(function () {
-  function current() {
-    var d = new Date(), str = '';
-    str += d.getFullYear() + '年'; //獲取當前年份 
-    str += d.getMonth() + 1 + '月'; //獲取當前月份（0——11） 
-    str += d.getDate() + '日';
-    str += d.getHours() + '時';
-    str += d.getMinutes() + '分';
-    str += d.getSeconds() + '秒';
-    return str;
-  }
-  setInterval(function () { $("#nowTime").html(current) }, 1000);
+
 });
-
-
-
-
